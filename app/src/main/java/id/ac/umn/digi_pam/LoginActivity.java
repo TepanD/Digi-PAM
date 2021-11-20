9bba734153b5b76b3d00892e6272a9b1b6700696
bagasandriann <bagasandriann@gmail.com>
2021-11-20 20:54:31 +0700

    add: LoginActivity Java

--- app/src/main/java/id/ac/umn/digi_pam/LoginActivity.java
+++ app/src/main/java/id/ac/umn/digi_pam/LoginActivity.java
@@ -13,15 +13,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     EditText inputEmail, inputPassword;
     String email, password;
     Button btnLogin;
     private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
                         } else {
                             // If sign in fails, display a message to the user.
-                            Toast.makeText(MainActivity.this, "Login failed, please check your username and password!!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "Login failed, please check your username and password!!", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
