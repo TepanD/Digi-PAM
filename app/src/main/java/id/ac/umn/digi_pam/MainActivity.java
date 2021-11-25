1b36255ff2a6af6bd64d68f002700916d3b730f7
Rizkyeps <rizky.ekaputra@student.umn.ac.id>
2021-11-25 14:42:49 +0700

    Add : SplashScreen, modified MainActivity, activity_main, form_login

--- app/src/main/java/id/ac/umn/digi_pam/MainActivity.java
+++ app/src/main/java/id/ac/umn/digi_pam/MainActivity.java
@@ -1,72 +1,31 @@
 package id.ac.umn.digi_pam;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    EditText inputEmail, inputPassword;
-    String email, password;
-    Button btnLogin;
-    private FirebaseAuth mAuth;
 
+    private static int SPLASH_SCREEN = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.form_login);
+        setContentView(R.layout.activity_main);
+
         getSupportActionBar().hide();
-        mAuth = FirebaseAuth.getInstance();
-        inputEmail = findViewById(R.id.etEmailLogin);
-        inputPassword = findViewById(R.id.etPasswordLogin);
-        btnLogin = findViewById(R.id.btn_login);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                cekLogin();
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
             }
-        });
-    }
+        }, SPLASH_SCREEN);
 
-    private void cekLogin() {
-        email = inputEmail.getText().toString();
-        password = inputPassword.getText().toString();
-
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(MainActivity.this, "Login Success"
-                                    , Toast.LENGTH_SHORT).show();
-
-                            Intent GoToHomePage = new Intent(MainActivity.this
-                                    , LandingPageActivity.class);
-
-                            startActivity(GoToHomePage);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(MainActivity.this, "Login failed, please check your username and password!!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 }
